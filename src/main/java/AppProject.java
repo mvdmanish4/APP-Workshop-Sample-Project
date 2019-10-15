a83f7b6c4fc76b4c005123d7b37e71abb7406dcc
Manish Varma Datla <manish.varma.datla@CMU-SVs-MacBook-Pro.local>
2019-10-14 19:32:18 -0700

    comments added

--- src/main/java/AppProject.java
+++ src/main/java/AppProject.java
@@ -4,7 +4,7 @@ public class AppProject {
 
     public static void main(String[] args){
         int n = 100, t1 = 0, t2 = 1;
-
+        //Adding comments
         System.out.print("Upto " + n + ": ");
         while (t1 <= n)
         {
